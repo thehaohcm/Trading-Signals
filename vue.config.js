1ac37794a9f9678f3be9298bc308293280e16564
Hao Nguyen <thehaohcm@yahoo.com.vn>
2025-03-01 21:08:47 +0700

    update the proxy path

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
-      '/tcanalysis': {
+      '/tcanalysis/v1/evaluation': {
         target: 'https://apipubaws.tcbs.com.vn',
         changeOrigin: true,
         pathRewrite: {
-          '^/tcanalysis': '/tcanalysis'
+          '^/tcanalysis/v1/evaluation': '/tcanalysis/v1/evaluation'
         }
       }
     }
