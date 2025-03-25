5f591935e9927c71668df865293c1b8bea2674b7
Hao Nguyen <ext-hao.nguyen@spdigital.sg>
2025-03-25 16:34:59 +0700

    fix highlight item

--- src/components/StockVn.vue
+++ src/components/StockVn.vue
@@ -57,7 +57,7 @@
               <table class="table table-striped">
                 <tbody>
                   <tr v-for="stock in filteredPotentialStocks" :key="stock.symbol"
-                    @click="selectedStock = { code: stock.symbol };" style="cursor: pointer;"
+                    @click="$nextTick(() => { selectedStock = { code: stock.symbol }; });" style="cursor: pointer;"
                     :class="{ 'highlighted-row': selectedStock && selectedStock.code === stock.symbol }">
                     <td style="text-align: left; width: 1%;">
                       <input type="checkbox" @click="toggleStock(stock.symbol)">
