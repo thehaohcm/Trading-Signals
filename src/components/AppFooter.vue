362d206ab393f7d4e9812eb3b76efb90fb4b055d
Hao Nguyen <thehaohcm@yahoo.com.vn>
2025-12-27 13:50:02 +0700

    move disclaimer to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,8 @@
 <template>
-    <footer class="mt-5 text-center text-white bg-dark py-3">Copyright © by Nguyen The Hao 2025. All rights reserved.</footer>
+    <footer class="mt-5 text-center text-white bg-dark py-3">
+      <p class="mb-2">Disclaimer: The information and indicators on this website reflect the owner's views and should not be taken as investment advice.</p>
+      <p class="mb-0">Copyright © by Nguyen The Hao 2025. All rights reserved.</p>
+    </footer>
 </template>
 
 <script>
