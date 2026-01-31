28b73a2b9a4e62394967cf81ac4d1833ba0765f3
Hao Nguyen <thehaohcm@yahoo.com.vn>
2026-01-31 20:46:29 +0700

    add menu and footer for HomeView

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="home-view container-fluid mt-3">
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <button class="nav-link active" id="rrg-tab" data-bs-toggle="tab" data-bs-target="#rrg" type="button" role="tab" aria-controls="rrg" aria-selected="true">RRG Chart</button>
-      </li>
-    </ul>
-    <div class="tab-content" id="myTabContent">
-      <div class="tab-pane fade show active" id="rrg" role="tabpanel" aria-labelledby="rrg-tab">
-        <div class="d-flex justify-content-center mt-3">
-            <img src="/assets_rrgchart" class="img-fluid" alt="RRG Chart" />
+  <div id="app" class="d-flex flex-column min-vh-100">
+    <NavBar />
+    <notifications />
+    <div class="home-view container mt-4 flex-grow-1">
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button class="nav-link active" id="rrg-tab" data-bs-toggle="tab" data-bs-target="#rrg" type="button" role="tab" aria-controls="rrg" aria-selected="true">RRG Chart</button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="rrg" role="tabpanel" aria-labelledby="rrg-tab">
+          <div class="d-flex justify-content-center mt-3">
+              <img src="/assets_rrgchart" class="img-fluid" alt="RRG Chart" />
+          </div>
         </div>
       </div>
     </div>
+    <AppFooter />
   </div>
 </template>
 
 <script>
+import NavBar from './NavBar.vue';
+import AppFooter  from './AppFooter.vue';
+
 export default {
   name: 'HomeView',
+  components: {
+    NavBar,
+    AppFooter,
+  },
 }
 </script>
 
 <style scoped>
-.home-view {
-    padding: 20px;
-}
+/* Scoped styles if necessary */
 </style>
