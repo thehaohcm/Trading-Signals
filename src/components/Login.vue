73c6c2ba6357da7201f71d944b76ce13b1afe566
Hao Nguyen <thehaohcm@yahoo.com.vn>
2026-02-01 14:59:30 +0700

    fix login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -64,7 +64,7 @@ export default {
       }
 
       try {
-        const response = await fetch('https://services.entrade.com.vn/dnse-auth-service/login', {
+        const response = await fetch('/dnse-auth-service/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
