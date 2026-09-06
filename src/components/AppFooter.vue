4ba4a9278fdd9cc0a044694cc9b5963f08e46c89
Hao Nguyen <thehaohcm@yahoo.com.vn>
2026-09-06 11:40:01 +0700

    fix: make copyright year dynamic in AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,18 @@
 <template>
     <footer class="mt-auto text-center text-white bg-dark py-3">
       <p class="mb-2">Disclaimer: The information and indicators on this website reflect the owner's views and should not be taken as investment advice.</p>
-      <p class="mb-0">Copyright © by Nguyen The Hao 2025. All rights reserved.</p>
+      <p class="mb-0">Copyright © by Nguyen The Hao {{ currentYear }}. All rights reserved.</p>
     </footer>
 </template>
 
 <script>
 export default {
   name: 'AppFooter',
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
+  }
 };
 </script>
 
